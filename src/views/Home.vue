044d65bd051144fc33bc541648aa9fc4419efafe
Jason-Monger-Cash <125292484+Jason-Monger-Cash@users.noreply.github.com>
2023-03-03 09:14:32 -0500

    card development
    adding views, and ratings, style modifications

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,17 @@
   <div>
     <div>
       <!--gy-4 bootstrap spacing between rows-->
-      <div class='row gy-4'>
+      <div class='row gy-4' data-masonry='{"percentPosition": true }'>
 
         <!--iterate through products-->
         <div 
           v-for="(product, index) in this.products" 
           :key="parseInt(index)"
-          class="col-sm-4 mb-3 mb-sm-0"
+          class="col-sm-3 mb-3 mb-sm-0"
           :class="{ inBag : isInBag(product)}">
           
           <!--card begins here-->
-          <div class="card mt-5 h-100"
+          <div class="card mt-5 h-90"
               @mouseover="
               hideById(`img${product.video_id}`); 
               showById(`vid${product.video_id}`);
@@ -20,46 +20,70 @@
               @mouseleave="
               showById(`img${product.video_id}`); 
               hideById(`vid${product.video_id}`); 
-              pauseVideo(`vid${product.video_id}`);"
-          >
-
+              pauseVideo(`vid${product.video_id}`);">
+            <!--photo-->
             <img :id="`img${product.video_id}`" 
               class="card-img-top"  
               :src="`https://bitporn.com/contents/videos_screenshots/0/${product.video_id}/preview.jpg`"  
               :alt="`${product.title}`"   
             />
-            <video class="card-img-top" style="display:none" :id="`vid${product.video_id}`"
-              :src="`https://bitporn.com/get_file/1/550233d0323423424c9520074f287b19/0/196/196_preview.mp4/`" 
-              controls muted loop>
+            <!--video-->
+            <video class="card-img-top" 
+              style="display:none" 
+              :id="`vid${product.video_id}`"
+              :src="`${product.preview}`" 
+              muted loop controls
+              webkit-playsinline playsinline>
             </video>
+            
+            <!--stats-->
+            <div class="text-start text-light">
+            
+              <!--views font awesome eye-->
+              <i style="color: green;" class="fa-regular fa-eye p-2"></i>{{ product.video_viewed }}&nbsp;&nbsp;
+              <!--ratings font awesome heart-->
+              <i style="color: red;" class="fa-regular fa-heart"></i>&nbsp; &nbsp;{{ product.rating_amount }}
+            
+            </div>
 
             <!--card body-->
             <div class="card-body">
 
               <!--product title-->
-              <!--max 45 characters-->
-              <h5>{{product.title_short}}</h5>
-
-              <!--product price-->
-              <span style="color: yellow;
-                 font-size: larger;
-                 font-weight: bolder;">${{product.price_short}}
-                </span>
-
-            </div>
-
-            <div class="card-footer p-1">
-              <div class="row p-3">
+              <div class="text-start">
+                <h5>
+                  {{product.title_short}}
+                </h5>
+              </div>
+            </div><!--card body end-->
+            
+            <!--card footer-->
+            <div class="card-footer p-3">
+              
+              <!--card footer row-->
+              <div class="row p-1">
+                
+                <!--product price-->
+                <div class="text-end"
+                  style="color: yellow;
+                  font-size: larger;
+                  font-weight: bolder;
+                  padding-bottom: 5px;">${{product.price_short}}
+                </div>
+                  
+                <!--add to cart button-->
                 <button class="btn btn-primary"
                   v-if="product.inShoppingCart == 0" 
                   @click="addToBag(product)">Add to cart
                 </button>
+
+                <!--remove from cart button-->
                 <button v-else class="btn btn-outline-warning" 
                   @click="this.$store.dispatch('removeFromBag', product.video_id); 
                   product.inShoppingCart = 0;">Remove from cart
                 </button>
-              </div>
-            </div>
+              </div><!--card footer row end-->
+            </div><!--card footer end-->
           </div>
         </div>
       </div>
@@ -114,3 +138,21 @@ export default {
   }
 }
 </script>
+<style>
+
+.video {
+    object-fit: contain;
+    overflow-clip-margin: content-box; 
+    overflow: clip;
+}
+
+.preview {
+    display: inline-block;
+    position: relative;
+    left: 0;
+    height: auto;
+    top: 0;
+    width: calc(100% + 1px);
+}
+
+</style>

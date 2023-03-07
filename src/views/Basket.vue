1c1a51f8f6668f6cad9be5190b0f16411b5d9868
Jason-Monger-Cash <125292484+Jason-Monger-Cash@users.noreply.github.com>
2023-03-07 00:13:54 -0500

    major shopping cart styling overhaul

--- src/views/Basket.vue
+++ src/views/Basket.vue
@@ -4,87 +4,76 @@
   <template v-if="productsInBag.length">
 
     <!--shopping cart title-->
-    <h5 class="mb-1">Your Shopping Cart</h5>
-      
-    <div class='row'>
+    <h5 class="mb-4">Your Shopping Cart</h5>
+    <div class="container pb-5 py-0">
+      <div class='row d-flex justify-content-center'>
+        <div class="row cols-xl-5 pt-2 d-flex justify-content-center">
 
-      <!--iterate over products in shopping cart-->    
-      <div
-        v-for="(product, index) in productsInBag" 
-        :key="parseInt(index)"
-        class="col-sm-6 mb-3 mb-sm-0 mt-5">
-    
+        <!--iterate over products in shopping cart-->    
+        <div
+          v-for="(product, index) in productsInBag" 
+          :key="parseInt(index)"
+          class="col-sm-12 col-md-12 col-lg-7 col-xl-7 pb-2">
 
-        <!--card for each product begins here-->
-        <div 
-        class="card 
-        mt-5
-        h-80
-        ">
+          <!--card for each product begins here-->
+          <div class="card cart-border text-light">
 
-          <!--make the card light gray with white text-->
-          <div class="card-body text-light bg-secondary">
+              <!--remove product from cart-->
+              <div class="text-end"
+                @click="this.$store.dispatch('removeFromBag', product.video_id); 
+                product.inShoppingCart = 0;">
 
-            <!--remove product from cart-->
-            <div class="text-end"
-              @click="this.$store.dispatch('removeFromBag', product.video_id); 
-              product.inShoppingCart = 0;">
+                <!--red garbage can icon-->
+                <i class="fa-regular fa-trash-can text-danger fa-1x mx-2 mt-2 pb-2"></i>
+              </div>
 
-              <!--red garbage can icon-->
-              <i
-                style="border:1px solid red; border-radius:5px; padding:5px;" 
-                class="fa-regular 
-                fa-trash-can 
-                text-danger 
-                fa-2x
-                ">
-              </i>
-            </div>
+              <div class='row d-flex'>
 
-            <!--product image-->
-            <div class="mt-3">
-              <img class="rounded" 
-              :src="`https://bitporn.com/contents/videos_screenshots/0/${product.video_id}/preview.jpg`" 
-              width="320">
-            </div>
-          
-
-            <!---product title-->
-            <div class="footer mt-3 text-light bg-secondary">
-              <div>
-                  <h5>{{product.title}}</h5>
+              <!--product image-->
+              <div class="col text-start mx-2">
+                <img
+                :src="`https://bitporn.com/contents/videos_screenshots/0/${product.video_id}/preview.jpg`" 
+                width="160">
               </div>
             
-              <!---product price-->
-              <div class="text-end">
-                <h5 class="p-2">$ {{product.price}}</h5>
-              </div>
-            </div><!--end card body-->
-          </div><!--end footer-->
-        </div><!--end card-->
-      </div><!--end column-->
-    </div><!--end row-->
+
+              <!---product title-->
+              <div class="col d-flex text-light text-start mx-2">
+                <div>
+                    <h5>{{product.title}}</h5>
+                </div>
+
+                </div>
+              
+                <!---product price-->
+                <div class="text-end">
+                  <h5 class="p-2">$ {{product.price}}</h5>
+                </div>
+              
+            </div><!--end footer-->
+          </div><!--end card-->
+        </div><!--end column-->
+      
+
     
     <!--total, checkout or continue shopping-->
-    <div class="text-end mt-3">
+    <div class="col-sm-12 col-md-12 col-lg-7 col-xl-7 pb-3">
+    <div class="text-end mt-2">
 
       <!--total-->
-      <h2 style="padding-right: 15px;" class="text-light font-weight-bold font-size-lg">Total: $ {{orderTotal()}}</h2>
+      <h4 class="text-light">Total ${{orderTotal()}}</h4>
 
       <!--Buttons-->
-      <div class="mx-3">
-
-        <!--continue shopping-->
-        <button 
-          @click="this.$router.go(-1);"
-          class="btn btn-secondary btn-lg">
-          Continue Shopping
-        </button>&nbsp;&nbsp;
+      <div class="">
 
         <!--checkout button-->
-        <button class="btn btn-primary btn-lg">Checkout</button>
+        <button class="btn btn-primary btn-lg"><i class="fa-solid fa-cart-shopping"></i>&nbsp;Checkout</button>
       </div><!--end buttons-->
     </div><!--end total, checkout or continue shopping-->
+    </div>
+  </div>
+    </div>
+  </div><!--end row-->
 
   </template>
 
@@ -101,6 +90,24 @@ import { mapState } from 'vuex'
 
 export default {
   name: 'Basket',
+
+  methods: {
+    orderTotal() {
+      console.log(this.productsInBag);
+      var total = 0;
+      this.productsInBag.forEach(item => {
+        total += Number(item.price);
+      });
+      return total.toFixed(2)
+    }
+  },
+  computed: mapState([
+    'productsInBag' 
+  ]),
+}
+</script>
+
+
 
   methods: {
     orderTotal() {
